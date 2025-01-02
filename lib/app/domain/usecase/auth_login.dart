45e356f044a35b88093c49f34679ed8f45412c8c
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:24:09 +0700

    Update auth_login.dart

--- lib/app/domain/usecase/auth_login.dart
+++ lib/app/domain/usecase/auth_login.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/domain/entity/auth.dart';
-import 'package:dewakoding_kasir/app/domain/repository/auth_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
-import 'package:dewakoding_kasir/core/use_case/app_use_case.dart';
+import 'package:bintang_kasir/app/domain/entity/auth.dart';
+import 'package:bintang_kasir/app/domain/repository/auth_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/core/use_case/app_use_case.dart';
 
 class AuthLoginUseCase extends AppUseCase<Future<DataState>, AuthEntity> {
   final AuthRepository _authRepository;
