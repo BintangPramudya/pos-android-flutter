511399f0da6bd1a59d9046be40aad04ce6eb0c1a
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:21:19 +0700

    Update setting_repository.dart

--- lib/app/data/repository/setting_repository.dart
+++ lib/app/data/repository/setting_repository.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/data/source/setting_api_service.dart';
-import 'package:dewakoding_kasir/app/domain/entity/setting.dart';
-import 'package:dewakoding_kasir/app/domain/repository/setting_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/app/data/source/setting_api_service.dart';
+import 'package:bintang_kasir/app/domain/entity/setting.dart';
+import 'package:bintang_kasir/app/domain/repository/setting_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
 
 class SettingRepositoryImpl extends SettingRepository {
   final SettingApiService _settingApiService;
