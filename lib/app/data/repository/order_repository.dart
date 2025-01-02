a0be7cdc241c51448a8f48f0ddad4a91b4ac27cd
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:20:00 +0700

    Update order_repository.dart

--- lib/app/data/repository/order_repository.dart
+++ lib/app/data/repository/order_repository.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/data/source/order_api_service.dart';
-import 'package:dewakoding_kasir/app/domain/entity/order.dart';
-import 'package:dewakoding_kasir/app/domain/repository/order_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/app/data/source/order_api_service.dart';
+import 'package:bintang_kasir/app/domain/entity/order.dart';
+import 'package:bintang_kasir/app/domain/repository/order_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
 
 class OrderRepositoryImpl extends OrderRepository {
   final OrderApiService _orderApiService;
