2c601ad4296a53fb9d957eed66ef1299583a7b34
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:23:21 +0700

    Update order_repository.dart

--- lib/app/domain/repository/order_repository.dart
+++ lib/app/domain/repository/order_repository.dart
@@ -1,5 +1,5 @@
-import 'package:dewakoding_kasir/app/domain/entity/order.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/app/domain/entity/order.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
 
 abstract class OrderRepository {
   Future<DataState<List<OrderEntity>>> getAll();
