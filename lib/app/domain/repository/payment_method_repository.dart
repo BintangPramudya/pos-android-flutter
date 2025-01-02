fd90df01f1ddc8a7ed4b903e6e84b65f8e40ca49
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:23:32 +0700

    Update payment_method_repository.dart

--- lib/app/domain/repository/payment_method_repository.dart
+++ lib/app/domain/repository/payment_method_repository.dart
@@ -1,5 +1,5 @@
-import 'package:dewakoding_kasir/app/domain/entity/payment_method.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/app/domain/entity/payment_method.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
 
 abstract class PaymentMethodRepository {
   Future<DataState<List<PaymentMethodEntity>>> getAll();
