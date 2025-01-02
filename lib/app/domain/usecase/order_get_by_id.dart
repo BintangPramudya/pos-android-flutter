107453b91d14b01414785618d2cf825bb4db1ed6
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:24:34 +0700

    Update order_get_by_id.dart

--- lib/app/domain/usecase/order_get_by_id.dart
+++ lib/app/domain/usecase/order_get_by_id.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/domain/entity/order.dart';
-import 'package:dewakoding_kasir/app/domain/repository/order_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
-import 'package:dewakoding_kasir/core/use_case/app_use_case.dart';
+import 'package:bintang_kasir/app/domain/entity/order.dart';
+import 'package:bintang_kasir/app/domain/repository/order_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/core/use_case/app_use_case.dart';
 
 class OrderGetByIdUseCase
     extends AppUseCase<Future<DataState<OrderEntity>>, int> {
