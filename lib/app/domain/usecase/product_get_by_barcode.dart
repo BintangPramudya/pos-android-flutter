2f93ceae15684ca1459e3f93c90ddb68c84f4c32
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:25:33 +0700

    Update product_get_by_barcode.dart

--- lib/app/domain/usecase/product_get_by_barcode.dart
+++ lib/app/domain/usecase/product_get_by_barcode.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/domain/entity/product.dart';
-import 'package:dewakoding_kasir/app/domain/repository/product_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
-import 'package:dewakoding_kasir/core/use_case/app_use_case.dart';
+import 'package:bintang_kasir/app/domain/entity/product.dart';
+import 'package:bintang_kasir/app/domain/repository/product_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/core/use_case/app_use_case.dart';
 
 class ProductGetByBarcodeUseCase
     extends AppUseCase<Future<DataState<ProductEntity>>, String> {
