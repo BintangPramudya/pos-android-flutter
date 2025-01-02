92a200cc2ae2b71eda6588dc7a2e312ed60f6267
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:23:54 +0700

    Update setting_repository.dart

--- lib/app/domain/repository/setting_repository.dart
+++ lib/app/domain/repository/setting_repository.dart
@@ -1,5 +1,5 @@
-import 'package:dewakoding_kasir/app/domain/entity/setting.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/app/domain/entity/setting.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
 
 abstract class SettingRepository {
   Future<DataState<SettingEntity>> get();
