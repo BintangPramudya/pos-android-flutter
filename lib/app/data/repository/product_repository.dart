6ae331cc7bb8ad55ca043485223efc729a39a5f8
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:20:52 +0700

    Update product_repository.dart

--- lib/app/data/repository/product_repository.dart
+++ lib/app/data/repository/product_repository.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/data/source/product_api_service.dart';
-import 'package:dewakoding_kasir/app/domain/entity/product.dart';
-import 'package:dewakoding_kasir/app/domain/repository/product_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/app/data/source/product_api_service.dart';
+import 'package:bintang_kasir/app/domain/entity/product.dart';
+import 'package:bintang_kasir/app/domain/repository/product_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
 
 class ProductRepositoryImpl extends ProductRepository {
   final ProductApiService _productApiService;
