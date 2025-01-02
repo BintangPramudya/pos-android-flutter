143a9ea5a2f5f842a95e9a68924545a77ad2f45e
BintangPramudya <150113843+BintangPramudya@users.noreply.github.com>
2025-01-02 10:25:43 +0700

    Update setting_get.dart

--- lib/app/domain/usecase/setting_get.dart
+++ lib/app/domain/usecase/setting_get.dart
@@ -1,7 +1,7 @@
-import 'package:dewakoding_kasir/app/domain/entity/setting.dart';
-import 'package:dewakoding_kasir/app/domain/repository/setting_repository.dart';
-import 'package:dewakoding_kasir/core/network/data_state.dart';
-import 'package:dewakoding_kasir/core/use_case/app_use_case.dart';
+import 'package:bintang_kasir/app/domain/entity/setting.dart';
+import 'package:bintang_kasir/app/domain/repository/setting_repository.dart';
+import 'package:bintang_kasir/core/network/data_state.dart';
+import 'package:bintang_kasir/core/use_case/app_use_case.dart';
 
 class SettingGetUseCase
     extends AppUseCase<Future<DataState<SettingEntity>>, void> {
